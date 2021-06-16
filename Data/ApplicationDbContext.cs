320314942737ba4331ec078cce8af53a230af389
Oluwasegun <akinpelu53@gmail.com>
2021-06-16 12:25:31 +0100

    Code Along

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MyKoloWebApi.Models;
 
 namespace MyKoloWebApi.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext :DbContext
     {
-        
+        //DbSets to represent tables
+        //Type of generic collection
+        //DbSet<T>
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Savings> Savings { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
     }
 }
