86efa120d5a1e33582c9a11db85cc621ead77453
Oluwasegun Akinpelu <82780450+oluwasege@users.noreply.github.com>
2021-07-03 03:44:06 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace MyKoloWebApi
 {
@@ -29,6 +30,10 @@ namespace MyKoloWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+             services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Aduaba", Version = "v1" });
+            });
             services.AddControllers();
             services.AddDbContext<ApplicationDbContext>(options=>
             {
@@ -51,6 +56,8 @@ namespace MyKoloWebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+             app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Aduaba v1"));
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
